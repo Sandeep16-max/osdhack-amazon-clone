3d8971860cc956922ac0384148639992911c2448
Mohit Yadav <mohityadavhr36@gmail.com>
2023-04-23 14:59:37 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import Header from'./Header';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import Login from "./Login"
+import Header from "./Header";
+import Home from "./Home";
+import Payment from "./Payment"
+import Checkout from "./Checkout"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {auth} from "./firebase";
+import { useStateValue } from "./StateProvider";
+
+
 
 function App() {
+  const[{},dispatch] = useStateValue()
+  useEffect(()=>{
+    auth.onAuthStateChanged(authUser=>{
+      console.log('THE USER IS >>>', authUser);
+      if(authUser){
+        // user is logged in our just logged in 
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        })
+      }
+      else{
+        // the user is logged out 
+        dispatch({type: "SET_USER",
+        user: null
+      })
+      }
+    })
+  },[])
   return (
-    <div className="App">
-      
+    <BrowserRouter>
       <Header />
-    </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/payment" element={<Payment/>} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
